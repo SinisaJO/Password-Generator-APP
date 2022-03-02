7541e350ecd99d5ccfdb2a29924e64c160d5a906
Sinisa Jovanovic <73884960+SinisaJO@users.noreply.github.com>
2022-03-02 23:12:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ const green = "#10B981";
 const plusBtn = document.querySelector(".plus-btn");
 const minusBtn = document.querySelector(".minus-btn");
 
+const arr = ["`","~","!","@","#","$","%","^","&","*","(",")","_","+","|", "?","<",">",";",":","{","}","[","]","1","2","3","4","5","6","7","8","9","0","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","C","V","B","N","M","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+
+
 pwLength.disabled = true;
 pwLength.value = 8;
 
 function generatePassword() {
     let length = pwLength.value
-    let arr = ["`","~","!","@","#","$","%","^","&","*","(",")","_","+","|", "?","<",">",";",":","{","}","[","]","1","2","3","4","5","6","7","8","9","0","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","C","V","B","N","M","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
     let password = ""
     for (let i = 0; i < length; i++){
         let randomNumber = Math.floor(Math.random() * arr.length)
